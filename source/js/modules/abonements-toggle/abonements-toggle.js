290db9c37faa0ab05e801841487b11ba7a0e363f
mgDentist <gmi1987@gmail.com>
2023-11-14 15:44:00 +0400

    Уточняет abonements-toggle.js

--- source/js/modules/abonements-toggle/abonements-toggle.js
+++ source/js/modules/abonements-toggle/abonements-toggle.js
@@ -6,6 +6,13 @@ const takeNumberfromString = (text) => {
 export const abonementsToggle = () => {
   document.querySelectorAll('.months__button').forEach((button) => {
     button.addEventListener('click', (evt) => {
+      document.querySelectorAll('.months__item').forEach((item) => {
+        item.classList.remove('months__item--active');
+      });
+      const parentItem = button.closest('.months__item');
+      if (parentItem) {
+        parentItem.classList.add('months__item--active');
+      }
       const amount = takeNumberfromString(evt.target.textContent);
       document.querySelectorAll('[data-month]').forEach((span) => {
         if (span.dataset.month === amount) {
